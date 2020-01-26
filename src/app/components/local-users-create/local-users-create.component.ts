0ef38e8eb862bc1ad74060671ff3380afaf96aed
Manoj Kumar J <46069670+manozkumarj@users.noreply.github.com>
2020-01-26 13:26:25 +0530

    Changed the patter of Email REGEX
    Changed the patter of Email REGEX

--- src/app/components/local-users-create/local-users-create.component.ts
+++ src/app/components/local-users-create/local-users-create.component.ts
@@ -30,7 +30,7 @@ export class LocalUsersCreateComponent implements OnInit {
         ]),
         email: new FormControl("", [
           Validators.required,
-          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
         ]),
         password: new FormControl("", [
           Validators.required,
